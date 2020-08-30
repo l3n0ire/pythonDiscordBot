485357de738bd31b9628f8c46f8904b88493fa66
Colin Lin <colincool100@gmail.com>
2020-08-30 12:00:48 -0400

    add working but get broken

--- mongo.py
+++ mongo.py
@@ -1,12 +1,55 @@
 import pymongo
 from pymongo import MongoClient
+from collections import OrderedDict
 
-cluster = MongoClient("mongodb+srv://colincool100:colinspassword@manage.wwmlv.mongodb.net/<dbname>?retryWrites=true&w=majority")
+cluster = MongoClient("mongodb+srv://colincool100:colinspassword@manage.wwmlv.mongodb.net/Courses?retryWrites=true&w=majority")
 db = cluster["Manage"]
-collection = db["Tasks"]
+collection = db["Courses"]
+
+courseSchema = {"$jsonSchema":
+  {
+    "bsonType": "object",
+    "required":["courseCode","tasks"],
+    "properties":{
+        "courseCode":{
+            "bsonType":"string",
+            "description": "must be a string and is required"
+        },
+        "tasks":{
+            "bsonType": "array",
+            "items": {
+                "bsonType":"object",
+                "required": [ "desc", "dueDate",],
+                "properties":{
+                    "desc": {
+                    "bsonType": "string",
+                    "description": "must be a string and is required"
+                    },
+                    "dueDate": {
+                    "bsonType": "string",
+                    "description": "must be a string and is not required"
+                    },
+                    "status": {
+                    "bsonType": "bool",
+                    "description": "must be a boolean and is required"
+                    }
+                }
+            }
+        }
+    }
+  }
+}
 
 def addMongo(course,description):
-    collection.insert_one({"courseCode":course,"description":description})
+    cmd =OrderedDict([('collMod', 'Courses'),
+        ('validator', courseSchema),
+        ('validationLevel', 'moderate')])
+    db.command(cmd)
+    try:
+        collection.insert_one({"courseCode":course,
+        "tasks":[{"desc":description, "dueDate":"today" ,"status":False}]})
+    except:
+        print("oof it didnt work")
 
 def printMongo(course):
     # results is an array
@@ -16,10 +59,10 @@ def printMongo(course):
         for result in results:
             output=output+" "+result["courseCode"]+ ": "+result["description"]
     elif course !="":
-        results = collection.find({"courseCode":course})
+        targetCourse = collection.find({"courseCode":course})
         output=course+": "
-        for result in results:
-            output=output+" "+result["description"]
+        for task in targetCourse.tasks:
+            output=output+" "+task["desc"]
     
     # gives back json object
     
